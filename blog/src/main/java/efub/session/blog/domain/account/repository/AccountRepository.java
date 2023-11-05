8466481e5b0e10b5d1ddbd5306cbfb9280b51a65
Kwon Min A <min2010011@gmail.com>
2023-11-06 01:44:17 +0900

    feat: 이메일 기반 유저 정보 조회 메소드 추가

--- blog/src/main/java/efub/session/blog/domain/account/repository/AccountRepository.java
+++ blog/src/main/java/efub/session/blog/domain/account/repository/AccountRepository.java
@@ -1,8 +1,14 @@
 package efub.session.blog.domain.account.repository;
 
-import efub.session.blog.domain.account.domain.Account;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import efub.session.blog.domain.account.domain.Account;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
-    Boolean existsByEmail(String email);
+
+	Optional<Account> findByEmail(String email);
+
+	Boolean existsByEmail(String email);
 }
