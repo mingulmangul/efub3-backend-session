9a55cf291f86a33aceb3581f1ba97fa7f4cf48a0
Kwon Min A <min2010011@gmail.com>
2023-11-06 01:25:49 +0900

    feat: 회원가입 컨트롤러 추가

---
+++ blog/src/main/java/efub/session/blog/domain/auth/controller/AuthController.java
@@ -0,0 +1,30 @@
+package efub.session.blog.domain.auth.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import efub.session.blog.domain.account.domain.Account;
+import efub.session.blog.domain.auth.dto.request.SignUpRequestDto;
+import efub.session.blog.domain.auth.dto.response.SignUpResponseDto;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
+public class AuthController {
+
+	private final AuthService authService;
+
+	@PostMapping("/signup")
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public SignUpResponseDto signUp(@RequestBody @Valid final SignUpRequestDto requestDto) {
+		Account account = authService.signUp(requestDto);
+		return new SignUpResponseDto(account);
+	}
+}
